93b9db42a1e97fba4403141455fb4ac8d55a9186
eduardo <eduardo.paes@lektek.com.br>
2022-12-29 11:32:11 -0300

    feat: add serilog sample implementation

--- SerilogExample/Program.cs
+++ SerilogExample/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Serilog;
 
+namespace SerilogExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log_filename.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Starting program");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
